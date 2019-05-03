2cf3241873cc6ad898e8d46df37e3347ad9d4030
Ivan Ivanov <tyrant32@gmail.com>
2019-05-03 18:49:17 +0300

    Copyright

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <a href="https://ivan-ivanov.info" target="_blank">Ivan Ivanov</a>
+                    <a href="https://ivan-ivanov.info" target="_blank">Ivan Ivanov</a> &copy;
                 </div>
             </div>
         </div>
