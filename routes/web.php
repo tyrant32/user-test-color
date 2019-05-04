4e2168447e15afab5b220e68e98bca7540bcd1b8
Ivan Ivanov <tyrant32@gmail.com>
2019-05-04 15:46:15 +0300

    Copyright

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,8 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
+Route::get('/', 'UsersController@index')->name('home');
 Route::get('/home', 'UsersController@index')->name('home');
 Route::post('/ajax/users/list', 'UsersController@ajaxUsersList')->name('ajax.users.list');
