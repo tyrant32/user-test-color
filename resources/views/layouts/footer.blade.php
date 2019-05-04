1b4db84de5b2ef382863207046f62e7062afdef4
Ivan Ivanov <tyrant32@gmail.com>
2019-05-04 17:58:30 +0300

    Copyright

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,9 +2,18 @@
     <div class="navbar-default">
         <div class="container">
             <div class="row">
-                <div class="col-md-12">
-                    <a href="https://ivan-ivanov.info" target="_blank">Ivan Ivanov</a> &copy; {{ date('Y') }}
+                <div class="col-md-4">
+                    <div class="list-group">
+                        <a href="https://ivan-ivanov.info" class="list-group-item list-group-item-action" target="_blank">
+                            Ivan Ivanov &copy; {{ date('Y') }}
+                        </a>
+                        <a href="https://github.com/tyrant32/user-test-color" class="list-group-item list-group-item-action" target="_blank">
+                            GitHub Repository
+                        </a>
+                    </div>
                 </div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
             </div>
         </div>
     </div>
