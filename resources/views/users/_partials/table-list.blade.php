6a78eb107636c42a6e8e2234825b9216b224dae7
Ivan Ivanov <tyrant32@gmail.com>
2019-05-07 17:39:02 +0300

    Fixes

--- resources/views/users/_partials/table-list.blade.php
+++ resources/views/users/_partials/table-list.blade.php
@@ -10,39 +10,41 @@
 
     <div class="panel-body">
 
-        <table class="table table-striped table-inverse table-responsive">
-            <thead class="thead-inverse">
-            <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Favorite Colors</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($users as $user)
-                <tr id="user-{{ $user->id }}">
-                    <td>{{ $user->id }}</td>
-                    <td>{{ $user->first_name }}</td>
-                    <td>{{ $user->last_name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>
-                        @forelse($user->favoriteColors as $color)
-                            <span class="badge badge-primary">
+        <div class="table-responsive">
+            <table class="table table-striped table-inverse">
+                <thead class="thead-inverse">
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Favorite Colors</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($users as $user)
+                    <tr id="user-{{ $user->id }}">
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->first_name }}</td>
+                        <td>{{ $user->last_name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>
+                            @forelse($user->favoriteColors as $color)
+                                <span class="badge badge-primary">
                                 <span style="color:{{ $color->name }}">{{ $color->name }}</span>
                             </span>
-                        @empty
-                            --- No Colors ---
-                        @endforelse
-                    </td>
+                            @empty
+                                --- No Colors ---
+                            @endforelse
+                        </td>
+                    </tr>
+                @endforeach
+                <tr>
+                    <td colspan="5">{{$users->links()}}</td>
                 </tr>
-            @endforeach
-            <tr>
-                <td colspan="5">{{$users->links()}}</td>
-            </tr>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
 
     </div>
 </div>
